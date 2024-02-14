79f4d0b16fa7e653b62937cd2d2722b6ec3cf5c5
H3rzius <h3rzius@gmail.com>
2024-02-14 10:40:36 +0100

    fourth try to add project to repo (with gradle)

---
+++ src/main/java/de/figmaballs/figmaballsbackend/FigmaballsBackendApplication.java
@@ -0,0 +1,13 @@
+package de.figmaballs.figmaballsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigmaballsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FigmaballsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/figmaballs/figmaballsbackend/FigmaballsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.figmaballs.figmaballsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigmaballsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
