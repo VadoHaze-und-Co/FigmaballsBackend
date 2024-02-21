cc0dc85d5a0570558433ad8f7abbd090288f45c1
H3rzius <h3rzius@gmail.com>
2024-02-21 12:45:22 +0100

    implement swagger-ui api config

--- src/main/java/de/szut/lf8_project/controllers/TicketController.java
+++ src/main/java/de/szut/lf8_project/controllers/TicketController.java
@@ -2,23 +2,28 @@ package de.szut.lf8_project.controllers;
 
 import de.szut.lf8_project.dtos.CreateTicketDTO;
 import de.szut.lf8_project.dtos.GetTicketDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController("/tickets")
-@RequestMapping
+@RequestMapping("v3/api/figmaballs/tickets")
 public class TicketController {
 
     public TicketController() {
     }
 
-    @PostMapping("")
+    @PostMapping("/create")
     public GetTicketDTO create(@RequestBody @Valid CreateTicketDTO dto) {
         return null;
     }
 
-    @GetMapping("")
+    @GetMapping("/get")
     public String c() {
         return "kdoifd";
     }
