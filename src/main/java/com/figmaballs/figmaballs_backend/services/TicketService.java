55de590c2074e2652f565b8acfa16e0769377914
Corey Attireh <corey@uni-bremen.de>
2024-05-02 12:14:09 +0200

    scrum11

--- src/main/java/com/figmaballs/figmaballs_backend/services/TicketService.java
+++ src/main/java/com/figmaballs/figmaballs_backend/services/TicketService.java
@@ -66,4 +66,8 @@ public class TicketService {
     public TicketEntity readById(long id) {
         return this.repository.getOne(id);
     }
+
+    public void deleteById(long id) {
+        this.repository.deleteById(id);
+    }
 }
