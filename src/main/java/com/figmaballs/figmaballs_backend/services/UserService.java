d36e4725560d7d3570b8860fe0bb9361a853bddb
H3rzius <h3rzius@gmail.com>
2024-05-08 08:42:34 +0200

    add load users

--- src/main/java/com/figmaballs/figmaballs_backend/services/UserService.java
+++ src/main/java/com/figmaballs/figmaballs_backend/services/UserService.java
@@ -6,6 +6,7 @@ import com.figmaballs.figmaballs_backend.entities.UserEntity;
 import com.figmaballs.figmaballs_backend.repos.SettingRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,6 +17,32 @@ public class UserService {
     public UserService(UserRepository repository, SettingRepository settingRepository) {
         this.repository = repository;
         this.settingRepository = settingRepository;
+        loadUsers();
+    }
+
+    public void loadUsers() {
+        String[] usernames = {"maxmustermann","mariamusterfrau","abcdefgh","reinert-zufall"};
+        String[] firstName = {"Max","Maria","Admin","Reinert"};
+        String[] lastNames = {"Mustermann","Musterfrau","ABCDEFGH","Zufall"};
+        String[] emails = {"max.mustermann@email.com","maria.musterfrau@email.com","admin@email.com","reinert-zufall@email.com"};
+        String[] address = {"Irgendwo Str. 1","Irgendwo Str. 1","Firmaweg 99","Zufallsstraße 123"};
+        String[] postcodes = {"12345","12345","01254","98765"};
+        String[] cities = {"Musterstadt","Musterstadt","Firmenstadt","Zufallsdorf"};
+        String[] groupIds = {"2","2","5","1"};
+        boolean[] isAdmins = {false,false,true,false};
+        for (int i = 0; i < usernames.length; i++) {
+            UserEntity user = new UserEntity();
+            user.setUserName(usernames[i]);
+            user.setFirstName(firstName[i]);
+            user.setLastName(lastNames[i]);
+            user.setEmailAddress(emails[i]);
+            user.setAddress(address[i]);
+            user.setPostcode(postcodes[i]);
+            user.setCity(cities[i]);
+            user.setUserGroupIds(groupIds[i]);
+            user.setAdmin(isAdmins[i]);
+            this.repository.save(user);
+        }
     }
 
     public UserEntity create(UserEntity entity) {
